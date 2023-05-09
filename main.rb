3c7bbbb06a0438ced613f2017902c5566fe4d4a8
Matt Marini <mattmarini8765@gmail.com>
2023-05-10 08:18:03 +1000

    Game vs. computer only completed

--- main.rb
+++ main.rb
@@ -2,14 +2,14 @@ require 'colorize'
 
 # player's name, player's guess
 class Player
-  attr_reader :name
+  attr_reader :name, :array
 
   def initialize(name)
     @name = name
   end
 
   def guess(array)
-    puts 'Make your guess fuck-boy!'
+    puts 'Have a guess!'
     counter = 0
     until counter == 4
       num = gets.chomp
@@ -31,9 +31,9 @@ class Board
       '3'.green.swap
     when 4
       '4'.yellow.swap
-    when 5 
+    when 5
       '5'.cyan.swap
-    when 6 
+    when 6
       '6'.magenta.swap
     end
   end
@@ -47,11 +47,26 @@ class Board
     puts "#{array[0]} #{array[1]} #{array[2]} #{array[3]}" 
     puts '------------------'
   end
+
+  def show_markers(comp_array, player_array)
+    @marker_array = []
+    player_array.each_index do |index|
+      if comp_array[index] == player_array[index]
+        @marker_array.push('o'.black.swap)
+      elsif comp_array.include?(player_array[index]) && comp_array[index] != player_array[index]
+        @marker_array.push('o'.white.swap)
+      end
+    end
+    puts '------------------'
+    puts "#{@marker_array[0]} #{@marker_array[1]}"
+    puts "#{@marker_array[2]} #{@marker_array[3]}"
+    puts '------------------'
+  end
 end
 
 # Play the game. Get the guesses from the play and see if it matches the code. Provide markers
 class Game
-  
+
   def computer_code(array)
     counter = 0
     until counter == 4
@@ -61,29 +76,46 @@ class Game
     end
   end
 
-  def player_guess(array)
-    player.guess(array)
-    board.show_board(array)
+  def winner?(player_array, comp_array)
+    player_array == comp_array
+  end
+
+  def player_guess(player_array, comp_array, player)
+    count = 0
+    name = player.name
+    until count == 8
+      board = Board.new
+      player.guess(player_array)
+      guess = player_array.clone
+      p player_array
+      board.show_board(guess)
+      board.show_markers(comp_array, player_array)
+      if winner?(player_array, comp_array)
+        puts "#{name} wins!"
+        break
+      end
+      player_array.clear
+      count += 1
+    end
+  end
+
+  def loser_message(comp_array)
+    puts 'LOSER! Here was the code:'
+    board = Board.new
+    board.show_board(comp_array)
   end
 
   def play
+    comp_array = []
+    player_array = []
     puts "Let's play! enter your name"
     name = gets.chomp
     player = Player.new(name)
-    board = Board.new
-    comp_array = []
-    player_array = []
-    self.computer_code(comp_array)
-    player.guess(player_array)
-    board.show_board(player_array)
+    computer_code(comp_array)
+    player_guess(player_array, comp_array, player)
+    loser_message(comp_array)
   end
 end
 
 game = Game.new
 game.play
-
-
-# array = [1,2,3,4,5]
-
-# board = Board.new
-# board.show_board(array)
