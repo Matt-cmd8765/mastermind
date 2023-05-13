6f4a5352d4440a75b5f74a29100028f7c5166353
Matt Marini <mattmarini8765@gmail.com>
2023-05-13 13:24:30 +1000

    Game is done for now. Not perfect but white peg issue seems to be solved.

--- main.rb
+++ main.rb
@@ -2,17 +2,21 @@ require 'colorize'
 
 # player's name, player's guess
 class Player
-  attr_reader :name, :array
+  attr_reader :name
 
   def initialize(name)
     @name = name
   end
 
   def guess(array)
-    puts 'Have a guess!'
+    puts 'Have a guess! Pick 4 numbers between 1 and 6'
     counter = 0
     until counter == 4
       num = gets.chomp
+      until num.to_i.between?(1,6)
+        puts 'Please pick a number between 1 and 6 OR ELSE'
+        num = gets.chomp
+      end
       array.push(num.to_i)
       counter += 1
     end
@@ -48,15 +52,44 @@ class Board
     puts '------------------'
   end
 
-  def show_markers(comp_array, player_array)
-    @marker_array = []
-    player_array.each_index do |index|
-      if comp_array[index] == player_array[index]
-        @marker_array.push('o'.black.swap)
-      elsif comp_array.include?(player_array[index]) && comp_array[index] != player_array[index]
-        @marker_array.push('o'.white.swap)
+  def white(blacks, whites, master_array, marker_array)
+    white_array = whites.uniq
+    white_array.each do |num|
+      m = master_array.count(num)
+      b = blacks.count(num)
+      w = whites.count(num)
+      if m < w
+        result = m - b
+        result.times do
+          marker_array.push('o'.white.swap)
+        end
+      elsif w < m
+        result = m - w
+        result.times do
+          marker_array.push('o'.white.swap)
+        end
+      elsif w == m
+        result = w - b
+        result.times do
+          marker_array.push('o'.white.swap)
+        end
       end
     end
+  end
+
+  def show_markers(master_array, player_array)
+    @marker_array = []
+    @blacks = []
+    @whites = []
+    player_array.each_index do |index|
+      if master_array[index] == player_array[index]
+        @blacks.push(player_array[index])
+        @marker_array.push('o'.black.swap)
+      elsif master_array[index] != player_array[index] && master_array.include?(player_array[index])
+        @whites.push(player_array[index])
+      end
+    end
+    white(@blacks, @whites, master_array, @marker_array)
     puts '------------------'
     puts "#{@marker_array[0]} #{@marker_array[1]}"
     puts "#{@marker_array[2]} #{@marker_array[3]}"
@@ -76,44 +109,72 @@ class Game
     end
   end
 
-  def winner?(player_array, comp_array)
-    player_array == comp_array
+  def winner?(player_array, master_array)
+    player_array == master_array
   end
 
-  def player_guess(player_array, comp_array, player)
+  def player_guess(player_array, master_array, player)
     count = 0
     name = player.name
     until count == 12
       board = Board.new
       player.guess(player_array)
       guess = player_array.clone
-      p player_array
       board.show_board(guess)
-      board.show_markers(comp_array, player_array)
-      if winner?(player_array, comp_array)
+      board.show_markers(master_array, player_array)
+      if winner?(player_array, master_array)
         puts "#{name} wins!"
         break
       end
       player_array.clear
       count += 1
     end
+    if !winner?(player_array, master_array)
+      loser_message(master_array)
+    end
   end
 
-  def loser_message(comp_array)
+  def computer_guess(player_array, master_array)
+    count = 0
+    until count == 12
+      board = Board.new
+      computer_code(player_array)
+      guess = player_array.clone
+      board.show_board(guess)
+      board.show_markers(master_array, player_array)
+      if winner?(player_array, master_array)
+        puts 'Computer wins!'
+        break
+      end
+      player_array.clear
+      count += 1
+    end
+    if !winner?(player_array, master_array)
+      loser_message(master_array)
+    end
+  end
+
+  def loser_message(master_array)
     puts 'LOSER! Here was the code:'
     board = Board.new
-    board.show_board(comp_array)
+    board.show_board(master_array)
   end
 
   def play
-    comp_array = []
+    master_array = []
     player_array = []
     puts "Let's play! enter your name"
     name = gets.chomp
     player = Player.new(name)
-    computer_code(comp_array)
-    player_guess(player_array, comp_array, player)
-    loser_message(comp_array)
+    puts 'Would you like to be the code master(M) or guesser(G)?'
+    choice = gets.chomp
+    if choice == 'G'
+      computer_code(master_array)
+      player_guess(player_array, master_array, player)
+    elsif choice == 'M'
+      player.guess(master_array)
+      computer_guess(player_array, master_array)
+    end
   end
 end
 
